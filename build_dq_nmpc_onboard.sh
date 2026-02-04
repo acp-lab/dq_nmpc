f8b6a24f7f626bd7f30f07edcac75f184621624a
lfrecalde1 <lfrecalde1@espe.edu.ec>
2026-02-04 14:49:31 -0500

    update code

--- build_dq_nmpc_onboard.sh
+++ build_dq_nmpc_onboard.sh
@@ -10,20 +10,20 @@ platform_type=${platform_type:-race}
 echo 'thank you!'
 echo ""
 
-python3 dq_nmpc/dq_controller.py /home/eagle1/ws/acp_ws/src/acp-autonomy-stack/config/eagle/default/dq_control.yaml
+python3 dq_nmpc/dq_controller.py /home/eagle10/ws/acp_ws/src/acp-autonomy-stack/config/eagle/default/dq_control.yaml
 
 # Creating the folder where we are going to paste the files
-mkdir /home/eagle1/ws/acp_ws/install/dq_cpp/
-mkdir /home/eagle1/ws/acp_ws/install/dq_cpp/lib
+mkdir /home/eagle10/ws/acp_ws/install/dq_cpp/
+mkdir /home/eagle10/ws/acp_ws/install/dq_cpp/lib
 
-cp c_generated_code/libacados_ocp_solver_quadrotor.so /home/eagle1/ws/acp_ws/install/dq_cpp/lib
+cp c_generated_code/libacados_ocp_solver_quadrotor.so /home/eagle10/ws/acp_ws/install/dq_cpp/lib
 
 echo "Deleting old Files"
-rm -rf /home/eagle1/ws/acp_ws/src/dq_cpp/c_generated_code
-mv -f c_generated_code /home/eagle1/ws/acp_ws/src/dq_cpp/
+rm -rf /home/eagle10/ws/acp_ws/src/dq_cpp/c_generated_code
+mv -f c_generated_code /home/eagle10/ws/acp_ws/src/dq_cpp/
 
 
-cd /home/eagle1/ws/acp_ws
+cd /home/eagle10/ws/acp_ws
 source install/setup.bash
 colcon build --symlink-install --packages-select dq_cpp
 source install/setup.bash
