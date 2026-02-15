089c1917a45778954bea39caaf73840818973659
lfrecalde1 <lfrecalde1@espe.edu.ec>
2026-02-15 11:23:20 -0500

    clean worksapce

--- build_dq_nmpc_onboard.sh
+++ build_dq_nmpc_onboard.sh
@@ -10,20 +10,20 @@ platform_type=${platform_type:-race}
 echo 'thank you!'
 echo ""
 
-python3 dq_nmpc/dq_controller.py $COLCON_WS_DIR/src/acp-autonomy-stack/config/eagle/default/dq_control.yaml
+python3 dq_nmpc/dq_controller.py $COLCON_PAYLOAD_WS_DIR/src/acp-autonomy-stack/config/eagle/default/dq_control.yaml
 
 # Creating the folder where we are going to paste the files
-mkdir $COLCON_WS_DIR/install/dq_cpp/
-mkdir $COLCON_WS_DIR/install/dq_cpp/lib/
+mkdir $COLCON_PAYLOAD_WS_DIR/install/dq_cpp/
+mkdir $COLCON_PAYLOAD_WS_DIR/install/dq_cpp/lib/
 
-cp c_generated_code/libacados_ocp_solver_dq_quadrotor.so $COLCON_WS_DIR/install/dq_cpp/lib/
+cp c_generated_code/libacados_ocp_solver_dq_quadrotor.so $COLCON_PAYLOAD_WS_DIR/install/dq_cpp/lib/
 
 #echo "Deleting old Files"
-rm -rf $COLCON_WS_DIR/src/dq_cpp/c_generated_code
-mv -f c_generated_code $COLCON_WS_DIR/src/dq_cpp/
+rm -rf $COLCON_PAYLOAD_WS_DIR/src/dq_cpp/c_generated_code
+mv -f c_generated_code $COLCON_PAYLOAD_WS_DIR/src/dq_cpp/
 #
 #
-cd $COLCON_WS_DIR
-source $COLCON_WS_DIR/install/setup.bash
+cd $COLCON_PAYLOAD_WS_DIR
+source $COLCON_PAYLOAD_WS_DIR/install/setup.bash
 colcon build --symlink-install --packages-select dq_cpp
-source $COLCON_WS_DIR/install/setup.bash
+source $COLCON_PAYLOAD_WS_DIR/install/setup.bash
